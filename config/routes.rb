22bb87abc15f7e4ed4b77383eba865eb64ec5771
David Thomason <dlthomas@us.ibm.com>
2017-05-09 10:53:35 -0400

    Added Company controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'company#home'
+
+  get '/about', to: 'company#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/views/company/about.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe 'company/about.html.erb', type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
