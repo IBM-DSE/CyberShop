5b52313040fdf35a0988dc7cc08fe1514cca0af0
David Thomason <dlthomas@us.ibm.com>
2017-05-16 11:22:40 -0400

    Started CSV import functionality

--- app/admin/ml_scoring_param.rb
+++ app/admin/ml_scoring_param.rb
@@ -1,5 +1,17 @@
 ActiveAdmin.register MlScoringParam do
-  permit_params :name, ml_scoring_param_options_attributes: [:id, :value, :_destroy]
+  permit_params :file, :name, ml_scoring_param_options_attributes: [:id, :value, :_destroy]
+
+  # index do
+  #   form_tag import_csv_admin_ml_scoring_params_path, multipart: true do
+  #     file_field_tag :file
+  #     submit_tag 'Import'
+  #   end
+  # end
+
+  collection_action :import_csv, method: :post do
+    params[:file]
+    redirect_to collection_path, notice: 'CSV imported successfully!'
+  end
 
   show do
     attributes_table do
