80b4e8d52e68cd727896ee9fe5df4f172c9c1e22
David Thomason <dlthomas@us.ibm.com>
2017-05-16 13:40:19 -0400

    Removed param name upcase

--- app/models/ml_scoring_param.rb
+++ app/models/ml_scoring_param.rb
@@ -2,11 +2,6 @@ class MlScoringParam < ApplicationRecord
   has_many :ml_scoring_param_options, dependent: :destroy
   accepts_nested_attributes_for :ml_scoring_param_options, allow_destroy: true
   validates :name, presence: true
-  before_save :upcase_name
-
-  def upcase_name
-    self.name.upcase!
-  end
 
   def self.import(file)
 
