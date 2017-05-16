aa5f05a8fa9dd888bb5d57ede4b7f46011b21129
David Thomason <dlthomas@us.ibm.com>
2017-05-15 22:40:23 -0400

    Generates select_tag with options from database

--- app/helpers/scoring_helper.rb
+++ app/helpers/scoring_helper.rb
@@ -1,8 +1,20 @@
 module ScoringHelper
   def type in_type
     {
-      'string' => 'text',
+      'string'  => 'text',
       'integer' => 'number'
     }[in_type]
+  end
+
+  def scoring_input param
+    selectable_param = MlScoringParam.find_by_name(param['name'])
+    if !selectable_param
+      text_field_tag param['name'], nil, type: type(param['type'])
+    else
+      select_tag(param['name'],
+                 options_from_collection_for_select(selectable_param.ml_scoring_param_options,
+                                                    :value,
+                                                    :value))
+    end
   end
 end
