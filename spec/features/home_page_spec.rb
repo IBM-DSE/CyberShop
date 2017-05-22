fa2cc28c5fd6d81516febdbe8b6a46a5e8037401
David Thomason <dlthomas@us.ibm.com>
2017-05-22 14:02:06 -0400

    Added new company description

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,5 +4,6 @@ RSpec.feature 'Visiting the Home Page', type: :feature do
   scenario 'anonymous visitor sees the expected homepage content' do
     visit root_path
     expect(page).to have_text Rails.application.class.parent_name
+    expect(page).to have_text "The World's Smartest Consumer Electronics Retail Store"
   end
 end
