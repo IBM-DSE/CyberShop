80ffad67f15bfa9cb8c0fdf7a2dc9c72abe326be
David Thomason <dlthomas@us.ibm.com>
2017-05-23 14:53:52 -0400

    Added app_configured shared context for app_name method

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature 'Visiting the Home Page', type: :feature do
+  include_context 'app configured'
+
   scenario 'anonymous visitor sees the expected homepage content' do
     visit root_path
-    expect(page).to have_text Rails.application.class.parent_name
+    expect(page).to have_text app_name
     expect(page).to have_text "The World's Smartest Consumer Electronics Retail Store"
   end
 end
---
+++ spec/support/app_configured.rb
@@ -0,0 +1,26 @@
+RSpec.configure do |rspec|
+  # This config option will be enabled by default on RSpec 4,
+  # but for reasons of backwards compatibility, you have to
+  # set it on RSpec 3.
+  #
+  # It causes the host group and examples to inherit metadata
+  # from the shared context.
+  rspec.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+RSpec.shared_context 'app configured', shared_context: :metadata do
+  # before { @some_var = :some_value }
+
+  def app_name
+    Rails.application.class.parent_name
+  end
+
+  # let(:shared_let) { {'arbitrary' => 'object'} }
+  # subject do
+  #   'this is the subject'
+  # end
+end
+
+RSpec.configure do |rspec|
+  rspec.include_context 'app configured', include_shared: true
+end
