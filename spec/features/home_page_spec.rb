aca6a534ec491ff3bec52db50a493c690237f732
David Thomason <dlthomas@us.ibm.com>
2017-05-23 16:05:09 -0400

    Added homepage tabs

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -7,5 +7,11 @@ RSpec.feature 'Visiting the Home Page', type: :feature do
     visit root_path
     expect(page).to have_text app_name
     expect(page).to have_text "The World's Smartest Consumer Electronics Retail Store"
+
+    expect(page).to have_selector '#nav.nav.nav-tabs'
+    nav = find('#nav')
+    expect(nav).to have_link 'Categories', href: '#'
+    expect(nav).to have_link 'Brands', href: '#'
+    expect(nav).to have_link 'Deals', href: '#'
   end
 end
