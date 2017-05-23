bd5aa4cbde7bdf6bc9b8239c72446b0b1eb77ebd
David Thomason <dlthomas@us.ibm.com>
2017-05-23 10:30:54 -0400

    Updated admin_spec

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -5,11 +5,17 @@ RSpec.feature 'Admin', type: :feature do
     visit '/admin'
 
     expect(page).to have_text 'You need to sign in or sign up before continuing.'
+    expect(page).to have_text 'Cyber World Login'
 
-    fill_in 'Email', :with => 'admin@example.com'
-    fill_in 'Password', :with => 'password'
+    page.fill_in 'Email*', with: 'admin@example.com'
+    page.fill_in 'Password*', with: 'password'
     click_button 'Login'
 
-    expect(page).to have_text 'Cyber World'
+    expect(find('#site_title')).to have_text 'Cyber World'
+    expect(find('#tabs')).to have_text 'Dashboard'
+    expect(find('#tabs')).to have_text 'Admin Users'
+    expect(find('#tabs')).to have_text 'Machine Learning Services'
+    expect(find('#tabs')).to have_text 'Ml Scoring Params'
+    expect(find('#tabs')).to have_text 'Scoring'
   end
 end
