dcaa86ea0e0d6d65cf4483cf8d09c0ea42feb8f8
David Thomason <dlthomas@us.ibm.com>
2017-05-23 14:42:36 -0400

    Removed extraneous specs

--- spec/models/ml_scoring_param_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe MlScoringParam, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/company/home.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'company/home.html.erb', type: :view do
-  it 'displays the title' do
-    render
-    expect(rendered).to match Rails.application.class.parent_name
-  end
-end
