43306cd149a91fd1873ba8f056bbcabcc9c21b97
David Thomason <dlthomas@us.ibm.com>
2017-05-23 16:26:06 -0400

    Added dropdown for categories

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -8,10 +8,21 @@ RSpec.feature 'Visiting the Home Page', type: :feature do
     expect(page).to have_text app_name
     expect(page).to have_text "The World's Smartest Consumer Electronics Retail Store"
 
-    expect(page).to have_selector '#nav.nav.nav-tabs'
-    nav = find('#nav')
-    expect(nav).to have_link 'Categories', href: '#'
-    expect(nav).to have_link 'Brands', href: '#'
-    expect(nav).to have_link 'Deals', href: '#'
+    within('#nav.nav.nav-tabs') do
+
+      within('li#categories.dropdown') do
+        expect(page).to have_link 'Categories', href: '#'
+        expect(page).to have_selector 'ul.dropdown-menu'
+        within('ul.dropdown-menu') do
+          expect(page).to have_link 'Category 1', href: '#'
+          expect(page).to have_link 'Category 2', href: '#'
+          expect(page).to have_link 'Category 3', href: '#'
+        end
+      end
+
+      expect(page).to have_link 'Brands', href: '#'
+      expect(page).to have_link 'Deals', href: '#'
+    end
+
   end
 end
