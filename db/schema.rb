fec78d0e1894d8cb4eb2f35db7cdd1271e8fa33e
David Thomason <dlthomas@us.ibm.com>
2017-05-24 12:10:51 -0400

    Created Brand model, registered in active_admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524142434) do
+ActiveRecord::Schema.define(version: 20170524160446) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20170524142434) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "categories", force: :cascade do |t|
