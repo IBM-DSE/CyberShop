1457e54b3c447bcda7baf6d5902b5ad8a78cffa0
David Thomason <dlthomas@us.ibm.com>
2017-05-24 16:33:46 -0400

    Added preorder column and Carousel to home

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524161411) do
+ActiveRecord::Schema.define(version: 20170524183859) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20170524161411) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.integer "brand_id"
+    t.boolean "preorder"
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
