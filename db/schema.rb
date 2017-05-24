966f41d8b10dee5dc8d9855fe41c9ec664e90158
David Thomason <dlthomas@us.ibm.com>
2017-05-24 10:27:59 -0400

    Removed image column from products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524140956) do
+ActiveRecord::Schema.define(version: 20170524142434) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -79,7 +79,6 @@ ActiveRecord::Schema.define(version: 20170524140956) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "category_id"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_file_name"
