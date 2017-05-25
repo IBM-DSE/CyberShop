46b49947c8594e46c87f59ff69c9412d73b5baa7
David Thomason <dlthomas@us.ibm.com>
2017-05-25 12:42:36 -0400

    Added categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
+  root 'company#home'
+
+  get '/about', to: 'company#about'
+
+  resources :categories, only: :show
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   namespace :admin do
     post 'scoring/score'
   end
-
-  root 'company#home'
-
-  get '/about', to: 'company#about'
 
 end
