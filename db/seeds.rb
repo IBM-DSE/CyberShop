585ddb40d26f87fa257cbd8bf8bf6eed94b7c639
David Thomason <dlthomas@us.ibm.com>
2017-05-26 14:21:51 -0400

    Added seeds for categories and brands

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+%w(Smartphones Laptops Desktops).each { |name| Category.create! name: name }
+%w(Apricot Gazillion Smithsong).each { |name| Brand.create! name: name }
