c47ef7df88e765e1147fb917160fbac6808d1494
David Thomason <dlthomas@us.ibm.com>
2017-06-03 21:49:02 -0400

    Added paperclip and migrated to create schema

--- Gemfile
+++ Gemfile
@@ -45,3 +45,4 @@ group :development do
   gem 'spring'
 end
 
+gem 'paperclip', '~> 5.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,112 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170527232221) do
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.string "slug"
+  end
+
+  add_index "brands", ["slug"], name: "index_brands_on_slug", unique: true
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "slug"
+  end
+
+  add_index "categories", ["slug"], name: "index_categories_on_slug", unique: true
+
+  create_table "deals", force: :cascade do |t|
+    t.integer  "product_id"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "deployments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "status"
+    t.integer  "prefix"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "machine_learning_service_id"
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
+  create_table "machine_learning_services", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+  end
+
+  create_table "ml_scoring_param_options", force: :cascade do |t|
+    t.integer "ml_scoring_param_id"
+    t.string  "value"
+  end
+
+  create_table "ml_scoring_params", force: :cascade do |t|
+    t.string "name"
+  end
+
+  add_index "ml_scoring_params", ["name"], name: "index_ml_scoring_params_on_name"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.integer  "brand_id"
+    t.boolean  "preorder"
+  end
 
 end
