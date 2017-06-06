25df18a735d72d87ca6b73e02b2e5b2624d2dd81
David Thomason <dlthomas@us.ibm.com>
2017-06-06 16:26:45 -0400

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,27 @@
 AdminUser.create! email: 'admin@example.com', password: 'password', password_confirmation: 'password'
 
 %w(Smartphones Laptops Desktops).each { |name| Category.create! name: name }
-%w(Apricot Gazillion Smithsong).each { |name| Brand.create! name: name }
+%w(Apricot Gazillion Singsong).each { |name| Brand.create! name: name }
 
 Customer.create! name: 'John Smith'
+
+Product.create! name: 'aPhone 9',
+                category: Category.find_by_name('Smartphones'),
+                brand: Brand.find_by_name('Apricot'),
+                image: File.new(Rails.root.join('public', 'aPhone9.png'), 'r')
+
+Product.create! name: 'aPhone 10',
+                category: Category.find_by_name('Smartphones'),
+                brand: Brand.find_by_name('Apricot'),
+                image: File.new(Rails.root.join('public', 'aPhone10.png'), 'r'),
+                preorder: true
+
+Product.create! name: 'FujiBook',
+                category: Category.find_by_name('Laptops'),
+                brand: Brand.find_by_name('Apricot'),
+                image: File.new(Rails.root.join('public', 'FujiBook.jpg'), 'r')
+
+Product.create! name: 'sPhone 8',
+                category: Category.find_by_name('Smartphones'),
+                brand: Brand.find_by_name('Singsong'),
+                image: File.new(Rails.root.join('public', 'sPhone8.jpg'), 'r')
