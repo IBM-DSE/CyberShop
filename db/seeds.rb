d6add689ec79273fb6e93590044f15928a1e0d6c
David Thomason <dlthomas@us.ibm.com>
2017-06-06 16:50:41 -0400

    Images work better

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,20 +15,26 @@ Customer.create! name: 'John Smith'
 Product.create! name: 'aPhone 9',
                 category: Category.find_by_name('Smartphones'),
                 brand: Brand.find_by_name('Apricot'),
-                image: File.new(Rails.root.join('public', 'aPhone9.png'), 'r')
+                image: File.new(Rails.root.join('public', 'images', 'aPhone9.png'), 'r')
 
-Product.create! name: 'aPhone 10',
+aPhone10 = Product.create! name: 'aPhone 10',
                 category: Category.find_by_name('Smartphones'),
                 brand: Brand.find_by_name('Apricot'),
-                image: File.new(Rails.root.join('public', 'aPhone10.png'), 'r'),
+                image: File.new(Rails.root.join('public', 'images', 'aPhone10-wide.jpg'), 'r'),
                 preorder: true
 
-Product.create! name: 'FujiBook',
+fujiBook = Product.create! name: 'FujiBook',
                 category: Category.find_by_name('Laptops'),
                 brand: Brand.find_by_name('Apricot'),
-                image: File.new(Rails.root.join('public', 'FujiBook.jpg'), 'r')
+                image: File.new(Rails.root.join('public', 'images', 'FujiBook.jpg'), 'r')
 
 Product.create! name: 'sPhone 8',
                 category: Category.find_by_name('Smartphones'),
                 brand: Brand.find_by_name('Singsong'),
-                image: File.new(Rails.root.join('public', 'sPhone8.jpg'), 'r')
+                image: File.new(Rails.root.join('public', 'images', 'sPhone8.jpg'), 'r')
+
+Deal.create! product: fujiBook,
+             description: 'Get a FREE set of headphones when you purchase FujiBook by Apricot!'
+
+Deal.create! product: aPhone10,
+             description: 'Pre-Order the upcoming aPhone 10 for just €100!'
