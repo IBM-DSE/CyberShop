21ca23b2d0509d3125667c70fdad50bd09c67d85
David Thomason <dlthomas@us.ibm.com>
2017-06-07 16:10:41 -0400

    Updated cart view

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,7 +1,10 @@
 module ProductsHelper
   
-  def order_button
-    link_to product_path
+  def stock_status(in_stock)
+    css_class = in_stock ? 'in-stock' : 'coming-soon'
+    content_tag :p, class: css_class do
+      in_stock ? 'In Stock' : 'Coming Soon!'
+    end
   end
   
 end
