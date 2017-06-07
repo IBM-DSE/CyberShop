a683c164b1db8820a1216911e85ad19afcdd4522
David Thomason <dlthomas@us.ibm.com>
2017-06-06 22:24:53 -0400

    Added deal image

--- app/admin/deal.rb
+++ app/admin/deal.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Deal do
-  permit_params :product_id, :description
+  permit_params :product_id, :description, :image
+  
+  form do |f|
+    f.semantic_errors
+    f.inputs :product, :description, :image
+    f.actions
+  end
+  
 end
