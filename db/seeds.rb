fffaa2c3ea0435a621208f1d549029e827fcba92
David Thomason <dlthomas@us.ibm.com>
2017-06-10 09:29:58 -0400

    Changed to Apricot Book, aPhone 8 pre-order is the featured deal

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,11 +24,11 @@ sPhone8 = Product.create! name:     'sPhone 8',
                           image:    File.new(Rails.root.join('public', 'images', 'sPhone8.jpg'), 'r'),
                           price:    799.00
 
-honeycrisp = Product.create! name:     'Honeycrisp Pro',
-                             category: Category.find_by_name('Laptops'),
-                             brand:    Brand.find_by_name('Apricot'),
-                             image:    File.new(Rails.root.join('public', 'images', 'FujiBook.jpg'), 'r'),
-                             price:    2299.00
+apricotBook = Product.create! name:     'Apricot Book',
+                              category: Category.find_by_name('Laptops'),
+                              brand:    Brand.find_by_name('Apricot'),
+                              image:    File.new(Rails.root.join('public', 'images', 'apricotbook.jpg'), 'r'),
+                              price:    2299.00
 
 aHeadphones = Product.create! name:     'Sounds by Sir Simon',
                               category: Category.find_by_name('Headphones'),
@@ -36,21 +36,21 @@ aHeadphones = Product.create! name:     'Sounds by Sir Simon',
                               image:    File.new(Rails.root.join('public', 'images', 'aHeadphones.jpg'), 'r'),
                               price:    299.00
 
-Deal.create! product:         aHeadphones,
-             description:     'Get a FREE pair of Sounds by Sir Simon headphones when you purchase Honeycrisp Pro by Apricot!',
-             trigger_product: honeycrisp,
-             price:           0.00
-
 Deal.create! product:     aPhone8,
-             description: 'Pre-Order the upcoming aPhone 8 for just €100!'
+             description: 'Pre-Order for just €100'
+
+Deal.create! product:         aHeadphones,
+             description:     'Get a FREE pair of Sounds by Sir Simon headphones when you purchase Apricot Book by Apricot!',
+             trigger_product: apricotBook,
+             price:           0.00
 
 Deal.create! product:     sPhone8,
              description: "Singsong's all-new sPhone8 has a voice assistant Boxy that can sing for you!"
 
 Deal.create! product:         aPhone8,
-             description:     'Good news! Because you are a loyal customer of the Apricot line and are about to purchase the Honeycrisp Book, we offer to waive the regular €100 Pre-Order fee. Add it to your cart right now for free!',
+             description:     'Good news! Because you are a loyal customer of the Apricot line and are about to purchase the Apricot Book, we offer to waive the regular €100 Pre-Order fee. Add it to your cart right now for free!',
              special:         true,
-             trigger_product: honeycrisp,
+             trigger_product: apricotBook,
              price:           0.00
 
 CSV.foreach('db/seed_data/WEX_output.csv', headers: true) do |row|
