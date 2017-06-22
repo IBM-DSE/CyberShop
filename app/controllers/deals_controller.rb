a57b5954753dda704f26b109c3185cc70d58008a
David Thomason <dlthomas@us.ibm.com>
2017-06-22 11:47:56 +0200

    Fewer deals and highlights

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,6 +1,6 @@
 class DealsController < ApplicationController
   def index
     @public_deals = Deal.where special: false
-    @deals = Deal.where special: false
+    @deals = Deal.where(special: false).where.not(trigger_product_id: nil)
   end
 end
