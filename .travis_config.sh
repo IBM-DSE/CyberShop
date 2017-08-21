c38f72a59024365cec6ff3eef044c667d72e9145
David Thomason <dlthomas@us.ibm.com>
2017-08-21 11:58:02 -0400

    Added .travis_config script to .travis.yml Updated .travis_config script: - added other Bluemix regions - switched from rails db:migrate to rake since Rails 4

--- .travis_config.sh
+++ .travis_config.sh
@@ -4,12 +4,16 @@ echo Travis Branch: $TRAVIS_BRANCH
 
 if [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-us-south-"* ]]; then
     APP_URL="https://cybershop.mybluemix.net/"
+elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-eu-gb-"* ]]; then
+    APP_URL="https://cybershop.eu-gb.mybluemix.net"
+elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-au-syd-"* ]]; then
+    APP_URL="https://cybershop.au-syd.mybluemix.net"
 fi
 
 if [ ! -z "$APP_URL" ]; then
     export APP_URL
 else
     APP_URL=localhost
-    bin/rails db:migrate RAILS_ENV=test
+    rake db:migrate RAILS_ENV=test
 fi
 echo Testing Target: $APP_URL
