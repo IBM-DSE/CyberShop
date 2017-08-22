88d4b3192890b7eb50b7ee1b796c0cbf4e6e8566
David Thomason <dlthomas@us.ibm.com>
2017-08-22 17:25:59 -0400

    Corrected Deals page failure

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -2,5 +2,6 @@ class DealsController < ApplicationController
   def index
     @public_deals = Deal.where special: false
     @deals = Deal.where(special: false).where.not(trigger_product_id: nil)
+    @deal = Deal.where(special: false).first
   end
 end
