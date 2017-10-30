26faa769ee40577105469a4b939801e792c78d58
David Thomason <dlthomas@us.ibm.com>
2017-10-30 15:36:15 -0400

    Show more clearly whether the endpoint is cloud or local

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -5,23 +5,26 @@ class StatusController < ApplicationController
       { product_name: 'sPhone 8', customer_name: 'David' }
     ]
     @products.each do |product|
-      
-      deployment                = Product.find_by_name(product[:product_name]).deployment
+
+      deployment = Product.find_by_name(product[:product_name]).deployment
+      ml_service = deployment.machine_learning_service
+
+      product[:ml_cloud] = ml_service.is_cloud?
       product[:deployment_guid] = deployment.guid
       product[:deployment_name] = deployment.name
-      product[:input_data] = deployment.input_data(Customer.find_by_name(product[:customer_name]))
+      product[:input_data]      = deployment.input_data(Customer.find_by_name(product[:customer_name]))
 
       begin
-        product[:output] = deployment.machine_learning_service.get_score deployment.guid, product[:input_data]
-        product[:score] = Util.extract product[:output], deployment.machine_learning_service.is_cloud?
+        product[:output] = ml_service.get_score deployment.guid, product[:input_data]
+        product[:score]  = Util.extract product[:output], deployment.machine_learning_service.is_cloud?
         product[:status] = 'green'
       rescue Exception => e
         STDERR.puts "#{e.class} : #{e.message}"
-        STDERR.puts e.backtrace.select{ |l| l.start_with? Rails.root.to_s }[0]
+        STDERR.puts e.backtrace.select {|l| l.start_with? Rails.root.to_s}[0]
         product[:output] = e
         product[:status] = 'failure'
       end
-      
+
     end
   end
 end
